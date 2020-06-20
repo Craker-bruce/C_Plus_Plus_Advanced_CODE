8c44882592a0d248ea83ef9f84c9bdeb2f974b0f
BranceCraker <1327968619@qq.com>
2020-06-21 01:45:23 +0800

    add 继承

---
+++ Test_06_20_inheritance/Test_06_20_inheritance/Test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+	void fun()
+	{
+		cout<<
+	}
+};
+
+//×ÓÀà          ¸¸Àà
+class D : public Base   //¼Ì³Ð
+{
+
+};
